592eab933f355bd00dd9c3c90fc6198a6d358bbd
tiris <130333904+iris536756@users.noreply.github.com>
2024-05-06 09:24:27 +0000

    Refactor templates

--- module/handlebars.js
+++ module/handlebars.js
@@ -21,5 +21,28 @@ export class KgRegisterHelpers {
       return options.fn(this);
     });
 
+    //
+    preloadHandlebarsTemplates();
+
   }
+}
+
+/**
+ * Define a set of template paths to pre-load. Pre-loaded templates are compiled and cached for fast access when
+ * rendering. These paths will also be available as Handlebars partials by using the file name
+ * (e.g. "kamigakari.item-description").
+ * @returns {Promise}
+ */
+async function preloadHandlebarsTemplates() {
+  const partials = [
+    "sheet/item/parts/item-description.hbs",
+  ].map(path => `systems/kamigakari/templates/${path}`);
+
+  const paths = {};
+  for (const path of partials) {
+    paths[path.replace(".hbs", ".html")] = path;
+    paths[`kamigakari.${path.split("/").pop().replace(".hbs", "")}`] = path;
+  }
+
+  return loadTemplates(paths);
 }
